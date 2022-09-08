0e8ab1ead2867d9f5a969d4de70d52347490147d
Rizki Adi Saputra <ucupucupucup10@gmail.com>
2022-09-08 15:25:06 +0700

    add nodejs

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
+var admin = require("firebase-admin");
 const bodyParser = require('body-parser');
 const authorizationPwd = "riskiadi+";
-
 const app = express();
-const router = express.Router();
 const port = 2021;
+
+var serviceAccount = require("./firebaseadmin.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://telemonitoring-kanker-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+var db = admin.database();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
 app.use(function (req, res, next) {
     
     if(req.headers.authorization === authorizationPwd) {
         return next()
     }
-    
+
     if(req.headers.authorization != authorizationPwd && req.headers.authorization != undefined) {
         res.status(401).json({
             status: {
@@ -35,11 +40,23 @@ app.use(function (req, res, next) {
 
 //===[API Start]>>
 
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/front.html');
 });
 
+app.get('/read', (req, res) => {
+
+    var ref = db.ref("/schedule").orderByChild("jam");
+    ref.once("value", function(snapshot) {
+        res.status(200).json({
+            status: {
+                code: 200,
+                message: snapshot.val(),
+            }
+        });
+    })
+
+});
 
 //===[API End]>>
 
